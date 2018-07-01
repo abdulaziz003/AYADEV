984fd288f9b0c875e31e7f3e3da61a1b5d76046e
Abdulaziz Muhammed Al Bidani <abdulaziz-003@hotmail.com>
2018-07-02 01:54:04 +0300

    textAlign
    text Alignment in main.js

--- Yunus/main.js
+++ Yunus/main.js
@@ -5,12 +5,14 @@ function changeText() {
     title1.innerHTML = "اختبار الالوان";
     btnAr.innerHTML = "English";
     document.getElementById("mainHtml").style.direction = "rtl";
+    document.getElementById("myTabContent").style.textAlign = "right";
     flag = true;
   }
   else{
     title1.innerHTML = "True Color Test";
     btnAr.innerHTML = "عربي";
     document.getElementById("mainHtml").style.direction = "ltr";
+    document.getElementById("myTabContent").style.textAlign = "left";
     flag = false;
   }
 }
